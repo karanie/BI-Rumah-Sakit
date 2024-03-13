2b23ef2ca9abc6f4f45cd546143472a3d5cd198b
Jacky Chandra <80088844+jackychandra29@users.noreply.github.com>
2024-03-13 16:16:31 +0700

    Filter penjamin

--- pages/data/penjamin.vue
+++ pages/data/penjamin.vue
@@ -2,14 +2,16 @@
   <Card>
     <template #title>Distribusi Jenis Penjamin</template>
     <template #content>
-      <Chart type="doughnut" :data="barChartData" />
+      <div style="width: 100%; display: flex; justify-content: center;">
+        <Chart type="doughnut" :options="{ showAllTooltips: true }" :data="barChartData" />
+      </div>
     </template>
   </Card>
 
   <Card>
     <template #title>10 Instansi Terfavorit</template>
     <template #content>
-      <Chart type="bar" :data="instansiChartData" />
+      <Chart type="bar" :options="{indexAxis: 'y'}" :data="instansiChartData" />
     </template>
   </Card>
 </template>
@@ -22,11 +24,24 @@ definePageMeta({
   layout: "data",
 });
 
+const {
+  bulan,
+  tahun,
+  kabupaten,
+  lastFilter,
+} = storeToRefs(useDataFilter())
+
 const barChartData = ref(null);
 const instansiChartData = ref(null);
 
 onMounted(async () => {
-  const response = await axios.get('http://localhost:5000/api/penjamin');
+  const response = await axios.get('http://localhost:5000/api/penjamin', {
+    params: {
+      tahun: tahun.value,
+      bulan: bulan.value,
+      kabupaten: kabupaten.value,
+    }
+  });
   const data = response.data;
 
   // Proses data untuk format grafik batang
@@ -43,7 +58,13 @@ onMounted(async () => {
 });
 
 onMounted(async () => {
-  const response = await axios.get('http://localhost:5000/api/instansi');
+  const response = await axios.get('http://localhost:5000/api/instansi', {
+    params: {
+      tahun: tahun.value,
+      bulan: bulan.value,
+      kabupaten: kabupaten.value,
+    }
+  });
   const data = response.data;
 
   const limitedData = {
@@ -63,4 +84,53 @@ onMounted(async () => {
     ]
   };
 });
+
+watch(lastFilter, async () => {
+  const response = await axios.get('http://localhost:5000/api/penjamin', {
+    params: {
+      tahun: tahun.value,
+    }
+  });
+  const data = response.data;
+
+  // Proses data untuk format grafik batang
+  barChartData.value = {
+    labels: data.index,
+    datasets: [
+      {
+        label: 'Jumlah',
+        data: data.values,
+        borderWidth: 1 // Lebar garis batas
+      }
+    ]
+  };
+})
+
+watch(lastFilter, async () => {
+  const response = await axios.get('http://localhost:5000/api/instansi', {
+    params: {
+      tahun: tahun.value,
+      bulan: bulan.value,
+      kabupaten: kabupaten.value,
+    }
+  });
+  const data = response.data;
+
+  const limitedData = {
+    index: data.index.slice(0, 10),
+    values: data.values.slice(0, 10)
+  };
+
+  // Proses data untuk format grafik batang
+  instansiChartData.value = {
+    labels: limitedData.index,
+    datasets: [
+      {
+        label: 'Jumlah',
+        data: limitedData.values,
+        borderWidth: 1 // Lebar garis batas
+      }
+    ]
+  };
+})
 </script>
