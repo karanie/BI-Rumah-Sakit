dffa79ddf4684867cc3ece2a5adac1dd2c1eb783
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-03-15 09:40:51 +0700

    Add post API `/api/register`

---
+++ server/api/register.post.ts
@@ -0,0 +1,30 @@
+import pgclient from "@/utils/db.server"
+import bcrypt from "bcrypt";
+
+const config = useRuntimeConfig();
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const password = bcrypt.hashSync(body.password, config.saltRounds);
+  const res = await pgclient.query(
+    `
+    INSERT INTO "user"(
+      username, nama_lengkap, jabatan, "role", "password", no_hp
+    ) VALUES(
+      $1, $2, $3, $4, $5, $6
+    )
+    `,
+    [
+      body.username,
+      body.nama_lengkap,
+      body.jabatan,
+      body.role,
+      password,
+      body.no_hp,
+    ]
+  );
+
+  return {
+    message: "Success",
+  };
+});
