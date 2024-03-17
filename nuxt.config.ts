7979e8ef8e45c9bde365567ca335139f27ae143c
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-03-17 21:43:33 +0700

    Configure `@sidebase/nuxt-auth` and set tokenSecret key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,28 @@ export default defineNuxtConfig({
     BIApiBase: process.env.BI_API_BASE ? process.env.BI_API_BASE : "http://localhost:5000",
     BIDatabaseAddr: process.env.BI_DATABASE_ADDR ? process.env.BI_DATABASE_ADDR : "localhost",
     saltRounds: 11,
+    tokenSecret: "VVVVT09PR0hIIENVTk5ZWS4uLi4hISEgOnNvYjo=",
   },
   modules: [
     'nuxt-primevue',
     'nuxt-icon',
     '@pinia/nuxt',
+    '@sidebase/nuxt-auth',
   ],
   primevue: {
     /* option */
+  },
+  auth: {
+    provider: {
+      type: 'local',
+      sessionDataType: {
+        user_id: 'number',
+        username: 'string',
+        nama_lengkap: 'string',
+        jabatan: 'string',
+        role: 'admin | non_admin',
+      }
+    }
   },
   css: [
     'primevue/resources/themes/aura-light-blue/theme.css',
