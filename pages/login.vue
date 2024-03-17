b69e5f6335d4d6d61912e7874328f542bc3c84d6
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-03-17 21:45:53 +0700

    Add login page

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,7 @@
     <div class="form__message">
       Silahkan login dengan username dan password anda.
     </div>
-    <div class="form__content">
+    <form @submit.prevent="login" class="form__content">
       <FloatLabel>
         <InputText id="username" v-model="username" />
         <label for="username">Username</label>
@@ -16,19 +16,33 @@
         <NuxtLink to="/register">
           <Button class="form-button__button" label="Register" outlined />
         </NuxtLink>
-        <Button class="form-button__button" label="Login" />
+        <Button type="submit" class="form-button__button" label="Login" />
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: "loginregis",
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/'
+  },
 });
 
+const { signIn } = useAuth();
 const username = ref();
 const password = ref();
+
+async function login() {
+  await signIn({
+    username: username.value,
+    password: password.value,
+  }, {
+    callbackUrl: "/",
+  });
+}
 </script>
 
 <style scoped lang="scss">
