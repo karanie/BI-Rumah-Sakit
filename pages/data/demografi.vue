a21c70a051c464c57e150ce8e4deec59e59c0239
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-03-28 10:52:12 +0700

    Simplify demografi page

--- pages/data/demografi.vue
+++ pages/data/demografi.vue
@@ -1,5 +1,13 @@
 <template>
   <Card>
+  <template #title>Demografi Riau</template>
+  <template #content>
+    <Skeleton height="8rem" v-if="dataIsPending" />
+    <GeoChart :data="demografiChartData1" />
+  </template>
+  </Card>
+
+  <Card>
   <template #title>Jumlah Pasien berdasarkan Kabupaten/Kota</template>
   <template #content>
     <Skeleton height="8rem" v-if="dataIsPending" />
@@ -8,26 +16,10 @@
   </Card>
 
   <Card>
-  <template #title>Top 10 Kabupaten/Kota</template>
-  <template #content>
-    <Skeleton height="8rem" v-if="dataIsPending" />
-    <Chart v-if="!dataIsPending" type="doughnut" :data="demografiChartData2" :options="demografiChartData2Opt" />
-  </template>
-  </Card>
-
-  <Card>
   <template #title>Kabupaten/Kota Seiring Waktu</template>
   <template #content>
     <Skeleton height="8rem" v-if="timeSeriesDataIsPending" />
-    <Chart v-if="!timeSeriesDataIsPending" id="asdf" type="line" :data="demografiChartData3" />
-  </template>
-  </Card>
-
-  <Card>
-  <template #title>Demografi Riau</template>
-  <template #content>
-    <Skeleton height="8rem" v-if="dataIsPending" />
-    <GeoChart :data="demografiChartData1" />
+    <Chart v-if="!timeSeriesDataIsPending" id="asdf" type="line" :data="demografiChartData2" />
   </template>
   </Card>
 </template>
@@ -42,8 +34,6 @@ definePageMeta({
 const demografiChartData1 = ref();
 const demografiChartData1Opt = ref();
 const demografiChartData2 = ref();
-const demografiChartData2Opt = ref();
-const demografiChartData3 = ref();
 const {
   tahun,
   bulan,
@@ -82,17 +72,15 @@ watch(data, () => {
   if (!data.value) {
     return;
   }
-  demografiChartData1.value = setDemografiChartData(reduceData(data.value, 10));
+  demografiChartData1.value = setDemografiChartData(data.value);
   demografiChartData1Opt.value = setDemografiChartDataOpt();
-  demografiChartData2.value = setDemografiChartData(reduceData(data.value, 10, false));
-  demografiChartData2Opt.value = setDemografiChartDataOpt();
 });
 
 watch(timeSeriesData, () => {
   if (!timeSeriesData.value) {
     return;
   }
-  demografiChartData3.value = {
+  demografiChartData2.value = {
     labels: timeSeriesData.value.index,
     datasets: timeSeriesData.value.columns.map((val: string, i: number) => {
       return {
@@ -107,17 +95,6 @@ watch(lastFilter, () => {
   refreshData();
   refreshTimeSeriesData();
 });
-
-function reduceData(data: any, threshold=10, lainnya=true) {
-  data.index = data.index.slice(0, data.index.length > threshold ? -(data.index.length - threshold) : 0);
-  const totalLainnya = data.values.slice(0, data.index.length).reduce((total: number, i: number) => total + i);
-  data.values = data.values.slice(0, data.index.length);
-  if (lainnya) {
-    data.index.push("Lainnya");
-    data.values.push(totalLainnya);
-  }
-  return data
-}
 
 const setDemografiChartData = (data: any) => {
   return {
