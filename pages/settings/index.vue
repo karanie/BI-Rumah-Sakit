65d9bcf6ce7c6e1d56e60d5aa27cdd5658131d15
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-04-25 10:44:20 +0700

    Refactor the code to use settings layout instead

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,50 +1,36 @@
 <template>
-  <div class="settings-cont">
-    <div class="settings">
-
-      <div class="settings__section">
-        <div class="welcome">
-          <div class="welcome__pp">
-            <Icon name="material-symbols:account-circle" size="64px" />
+  <div class="settings__section">
+    <Menu :model="settings">
+      <template #item="{ item, props }">
+        <NuxtLink :to="item.route" class="list-box-item" v-bind="props.action">
+          <div class="list-box-item__leading">
+            <span :class="props.icon" />
+            <Icon :name="item.nuxtIcon" />
           </div>
-          <div class="welcome__content">
-            <span class="welcome__content_msg">Selamat datang ke pengaturan, <span class="welcome__content__msg__fullname">{{ data?.nama_lengkap }}</span></span><br>
+          <div class="list-box-item__content">
+            {{ item.label }}
           </div>
-        </div>
-      </div>
-
-      <div class="settings__section">
-        <div class="section-title">Pengaturan</div>
-        <Menu :model="settings">
-          <template #item="{ item, props }">
-            <NuxtLink :to="item.route" class="list-box-item" v-bind="props.action">
-              <div class="list-box-item__leading">
-                <span :class="props.icon" />
-                <Icon :name="item.nuxtIcon" />
-              </div>
-              <div class="list-box-item__content">
-                {{ item.label }}
-              </div>
-              <div class="list-box-item__trailing">
-                <Icon name="material-symbols:navigate-next" />
-              </div>
-            </NuxtLink>
-          </template>
-        </Menu>
-      </div>
-
-    </div>
+          <div class="list-box-item__trailing">
+            <Icon name="material-symbols:navigate-next" />
+          </div>
+        </NuxtLink>
+      </template>
+    </Menu>
   </div>
 </template>
 
 <script setup lang="ts">
+definePageMeta({
+  layout: "settings",
+});
+
 const { status, data, signOut } = useAuth();
 
 const settings = ref([
   {
     nuxtIcon: "material-symbols:person-add",
     label: "Buat akun baru",
-    route: "/register",
+    route: "/settings/users",
   },
   {
     nuxtIcon: "material-symbols:password-2",
@@ -55,46 +41,11 @@ const settings = ref([
 </script>
 
 <style lang="scss" scoped>
-.settings-cont {
-  display: flex;
-  justify-content: center;
-}
-
-.settings {
-  padding: 8px 0 8px 0;
-  width: 100%;
-  max-width: 800px;
-
-  &__section {
-    padding: 16px 0 0 0;
-
-    &:first-child {
-      padding: 0;
-    }
-  }
-}
-
 .section-title {
   font-size: 1.25rem;
   font-weight: 500;
   color: var(--text-color);
   padding-bottom: 0.5rem;
-}
-
-.welcome {
-  display: flex;
-  align-items: center;
-  gap: 16px;
-
-  &__content {
-    &__msg {
-      font-size: 1.25rem;
-      &__fullname {
-        font-weight: bold;
-        color: var(--text-primary-color);
-      }
-    }
-  }
 }
 
 .list-box-item {
