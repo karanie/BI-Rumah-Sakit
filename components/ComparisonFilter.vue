6845a39e1a7f95b72b0d823f8e2182971a53a5e3
FNG <chintyaang98@gmail.com>
2024-05-02 09:08:01 +0700

    comparison page

---
+++ components/ComparisonFilter.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="card flex justify-content-center">
+        <Button type="button" icon="pi pi-filter" label="Date Filter" @click="toggle" />
+
+        <OverlayPanel ref="op">
+            <div class="flex flex-column gap-5 w-25rem">
+                <div>
+                    <div>
+                        <RadioButton v-model="pizza" inputId="ingredient1" name="pizza" value="Cheese" />
+                        <label for="ingredient1" class="ml-2">Filter Relatif</label l>
+
+                        <div class="card flex justify-content-center">
+                            <CascadeSelect v-model="selectedCity" :options="countries" optionLabel="name"
+                                optionGroupLabel="name" :optionGroupChildren="['states']" style="min-width: 14rem"
+                                placeholder="Select a City">
+                            </CascadeSelect>
+                        </div>
+                    </div>
+                    <div>
+                        <RadioButton v-model="pizza" inputId="ingredient2" name="pizza" value="Mushroom" />
+                        <label for="ingredient2" class="ml-2">Filter Statis</label>
+
+                        <div class="card flex justify-content-center">
+                            <CascadeSelect v-model="selectedCity" :options="countries" optionLabel="name"
+                                optionGroupLabel="name" :optionGroupChildren="['states']" style="min-width: 14rem"
+                                placeholder="Select a City">
+                            </CascadeSelect>
+                        </div>
+                    </div>
+
+                    <div>
+                        <RadioButton v-model="pizza" inputId="ingredient3" name="pizza" value="Mushroom2" />
+                        <label for="ingredient3" class="ml-2">Filter Statis</label>
+
+                        <div class="card flex justify-content-center">
+                            <CascadeSelect v-model="selectedCity" :options="countries" optionLabel="name"
+                                optionGroupLabel="name" :optionGroupChildren="['states']" style="min-width: 14rem"
+                                placeholder="Select a City">
+                            </CascadeSelect>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </OverlayPanel>
+    </div>
+</template>
+
+<script setup>
+
+
+const pizza = ref();
+
+const op = ref();
+
+const toggle = (event) => {
+    op.value.toggle(event);
+}
+
+const selectedCity = ref();
+
+
+const countries = ref([
+    {
+        name: 'Hari',
+        code: 'AU',
+        states: [
+            { name: 'New South Wales' },
+            { name: 'Queensland' },
+        ]
+    },
+    {
+        name: 'Bulan',
+        code: 'CA',
+        states: [
+            { name: 'Quebec' },
+            { name: 'Ontario' }
+        ]
+    },
+    {
+        name: 'Kuarter',
+        code: 'US',
+        states: [
+            { name: 'California' },
+            { name: 'Florida' },
+            { name: 'Texas' }
+        ]
+    },
+    {
+        name: 'Tahun',
+        code: 'US',
+        states: [
+            { name: 'California' },
+            { name: 'Florida' },
+            { name: 'Texas' }
+        ]
+    }
+]);
+
+</script>
---
+++ pages/comparison.vue
@@ -0,0 +1,46 @@
+<template>
+    <Message class="message" :closable="false">
+        Data terakhir di-update pada
+        <b> xxxx</b>
+    </Message>
+    <Message class="message" :closable="false">
+        Waktu registrasi pasien terakhir dari tanggal
+        <b> xxxx </b>
+    </Message>
+
+    <div class="grid">
+        <div class="grid__item1">
+            <ComparisonFilter></ComparisonFilter>
+            <BIChart src="/api/demografi" type="line" tipeData="timeseries" style="height: 100%;">
+                <template #title>Data 1</template>
+            </BIChart>
+        </div>
+
+        <div class="grid__item2">
+            <ComparisonFilter></ComparisonFilter>
+            <BIChart src="/api/jeniskelamin" type="line" style="height: 100%;">
+                <template #title>Data 2</template>
+            </BIChart>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    &__item1 {
+        padding: 10px;
+    }
+
+    &__item2 {
+        padding: 10px;
+    }
+}
+</style>
