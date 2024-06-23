d05f228b2e6cd6302978d4010522f2e698346d65
Naatie <muhammadqarani@gmail.com>
2024-06-23 21:39:24 +0700

    Remove duplicate proxyUrl after merging auth

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -4,7 +4,6 @@ export default defineEventHandler({
   onRequest: [auth],
   handler: async (event) => {
     const runtimeConfig = useRuntimeConfig();
-    const proxyUrl = runtimeConfig.BIApiBase;
     const proxyUrl = runtimeConfig.biApiBase;
     const target = joinURL(proxyUrl, event.path);
     return proxyRequest(event, target);
