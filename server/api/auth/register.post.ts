7d2d22305594a164a41b3dd853e67a7dc6ff0100
Naatie <muhammadqarani@gmail.com>
2024-07-02 20:27:08 +0700

    Restrict /api/auth/register route by authenticating the user

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,28 +3,31 @@ import bcrypt from "bcrypt";
 
 const config = useRuntimeConfig();
 
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  const password = bcrypt.hashSync(body.password, config.saltRounds);
-  const res = await pgclient.query(
-    `
-    INSERT INTO "user"(
-      username, nama_lengkap, jabatan, "role", "password", no_hp
-    ) VALUES(
-      $1, $2, $3, $4, $5, $6
-    )
-    `,
-    [
-      body.username,
-      body.nama_lengkap,
-      body.jabatan,
-      body.role,
-      password,
-      body.no_hp,
-    ]
-  );
+export default defineEventHandler({
+  onRequest: [auth],
+  handler: async (event) => {
+    const body = await readBody(event);
+    const password = bcrypt.hashSync(body.password, config.saltRounds);
+    const res = await pgclient.query(
+      `
+      INSERT INTO "user"(
+        username, nama_lengkap, jabatan, "role", "password", no_hp
+      ) VALUES(
+        $1, $2, $3, $4, $5, $6
+      )
+      `,
+      [
+        body.username,
+        body.nama_lengkap,
+        body.jabatan,
+        body.role,
+        password,
+        body.no_hp,
+      ]
+    );
 
-  return {
-    message: "Success",
-  };
+    return {
+      message: "Success",
+    };
+  }
 });
